d115bfbd4a96fc4a8ec2782d9f2380155175da04
Joey Biotti <joey@MacBook-Pro-2.local>
2018-01-08 21:00:32 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <h1>Word Translator</h1>
+    <h4>Powered By Vue.js</h4>
+    <TranslateForm></TranslateForm>
+  </div>
+</template>
+
+<script>
+import TranslateForm from './components/TranslateForm'
+
+export default {
+  name: 'app',
+  components: {
+    TranslateForm
+  }
+}
+</script>
+
+<style>
+#app {
+
+}
+</style>
---
+++ src/components/TranslateForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="translateForm">
+    <form v-on:submit ="formSubmit">
+        <input type="text" v-model="textToTranslate" placeholder="Enter a word...">
+        <input type="submit" value="Translate">
+    </form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'translateForm',
+  data(){
+      return {
+          textToTranslate: ''
+      }
+  },
+  methods:{
+      formSubmit(e){
+          alert('Submitted');
+          e.preventDefault();
+      }
+  }
+}
+</script>
+
+<style>
+
+</style>
